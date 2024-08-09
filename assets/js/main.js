7675311970dc544e212eb4a3a4c98ee8f43959bf
Marvin Blum <marvin.blum@emvi.com>
2024-08-09 14:05:07 +0200

    Improved mobile navigation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("Hi!");
+console.log("Hi! Welcome to my website.");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const nav = document.querySelector("#nav");
+
+    document.querySelector(".burger").addEventListener("click", () => {
+        if (nav.classList.contains("open")) {
+            nav.classList.remove("open");
+        } else {
+            nav.classList.add("open");
+        }
+    });
+});
